ba8202d0ea095bb8bd263e635c664650022d03a3
Meteor <jason.chuang1027@gmail.com>
2024-01-06 23:15:12 +0800

    0106 last update

--- books/views.py
+++ books/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Book
+from django.shortcuts import render
 
 def books(request):
-    mybooks = Book.objects.all().values()
-    template = loader.get_template('all_books.html')
-    context = {
-        'mybooks': mybooks,
-    }
-    return HttpResponse(template.render(context, request))
+  mybooks = Book.objects.all().values()
+  template = loader.get_template('all_books.html')
+  context = {
+    'mybooks': mybooks,
+  }
+  return HttpResponse(template.render(context, request))
 
 def details(request, id):
   mybook = Book.objects.get(id=id)
@@ -19,5 +20,56 @@ def details(request, id):
   return HttpResponse(template.render(context, request))
 
 def main(request):
+  book1 = Book.objects.get(bookname="遊戲人生 01")
+  book2 = Book.objects.get(bookname="射鵰英雄傳(一)")
+  book3 = Book.objects.get(bookname="系統程式設計(上册)")
+  book4 = Book.objects.get(bookname="系統程式設計(下册)")
   template = loader.get_template('main.html')
-  return HttpResponse(template.render())
+  context = {
+    'book1': book1,
+    'book2': book2,
+    'book3': book3,
+    'book4': book4,
+  }
+  return HttpResponse(template.render(context))
+
+def borrow(request):
+  if request.method == 'POST':
+    isbn = request.POST.get('ISBN', '')
+
+    try:
+      book = Book.objects.get(ISBN=isbn)
+      b_title = book.bookname
+
+      if book.lastQuantity > 0:
+        book.lastQuantity -= 1
+        book.save()
+
+        message = f'已借出《{b_title}》'
+        return render(request, 'borrow.html', {'success': message})
+      else:
+        return render(request, 'borrow.html', {'error': '架上數量不足'})
+
+    except Book.DoesNotExist:
+      return render(request, 'borrow.html', {'error': '書籍不存在'})
+
+  return render(request,'borrow.html')
+
+def return_book(request):
+  if request.method == 'POST':
+    isbn = request.POST.get('ISBN', '')
+
+    try:
+      book = Book.objects.get(ISBN=isbn)
+      b_title = book.bookname
+
+      book.lastQuantity += 1
+      book.save()
+
+      message = f'已歸還《{b_title}》'
+      return render(request, 'return_book.html', {'success': message})
+
+    except Book.DoesNotExist:
+      return render(request, 'return_book.html', {'error': '書籍不存在'})
+
+  return render(request,'return_book.html')
